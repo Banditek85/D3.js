198fb818b4a06645ec1cec2d63d0f2a86ace48bf
mitjasusec@outlook.com <mitja.susec@codingmonkeys.net>
2021-08-10 14:18:09 +0200

    added basic tree

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const list = d3.select("#list");
 
-// We map the existing dom elements to particular dataset passed as parameter to 'data' function.
-// This also gives us access to methods to dynamically update the DOM based on passed data. DOM representation
-// is tied to particular dataset object which is also called 'data join'. Data function creates placeholder references for any elements in dataset that are not existing in initial selection and returns this selection. 
+/* We map the existing dom elements to particular dataset passed as parameter to 'data' function.
+This also gives us access to methods to dynamically update the DOM based on passed data. DOM representation
+is tied to particular dataset object which is also called 'data join'. Data function creates placeholder 
+references for any elements in dataset that are not existing in initial selection and returns this selection. */
 
 list.selectAll("li")
     .data([10,20,30,40])
@@ -23,9 +24,9 @@ const svg = d3.select("svg");
 svg.selectAll("rect")
 .data([{x: 20, y: 30, width: 50, height: 100, fill: "green"},
        {x: 50, y: 40, width: 10, height: 200, fill: "red"}])
-.enter() // D3 compares number of elements from 'selectall' call with number of elements
-         // from bound data set. Any leftover elements from dataset are stored in 'enter' selection.
-         // Similarly any missing elements are stored in 'exit' selection. 
+.enter() /* D3 compares number of elements from 'selectall' call with number of elements
+          from bound data set. Any leftover elements from dataset are stored in 'enter' selection.
+          Similarly any missing elements are stored in 'exit' selection. */
 .append("rect")
 .attr("x", r => r.x)
 .attr("y", r => r.y)
@@ -42,30 +43,42 @@ events_div.on("mouseover", function() {
 });
 
 // Scales and bar chart
-// When defining a scale, you define 2 type of ranges: 'domain' (input values) and 'range' (output values) where you define minimum and maximum values for each.
+
+// When defining a scale, you define 2 type of ranges: 'domain' (input values) and 'range' (output values) 
+// where you define minimum and maximum values for each.
 const widthScale = d3.scaleLinear()
     .domain([0,60])
     .range([0,400]);
 
+
 const colorScale = d3.scaleLinear()
     .domain([0,60])
-    .range(["lightblue", "blue"]);
+    .range(["lightblue", "darkblue"]);
 
-// Append method appends named element as last child of current selection and returns collection of these newly created element(s). attr function returns current selection.
+
+// creating axis
+const axis = d3.axisTop(widthScale);
+
+
+// Append method appends named element as last child of current selection and returns collection of these newly 
+// created element(s). attr function returns current selection.
 const scales = d3.select("#scales")
     .append("svg")
-        .attr("width", 400)
-        .attr("height", 200)
-    .append("g");
+        .attr("width", 420)
+        .attr("height", 220)
+    .append("g")
+    .attr("transform", "translate(10,20)")
+    .call(axis);
 
 const bars = [20, 40, 50, 60];
 
+// Define bars
 scales.selectAll("rect")
     .data(bars)
     .enter()
     .append("rect")
-// D3 scales are functions 
-    .attr("width", d => widthScale(d))
-    .attr("height", d => 49)
-    .attr("y", (d, i) => i * 50)
-    .attr("fill", (d) => colorScale(d));
+        .attr("width", d => widthScale(d)) // D3 scales are functions 
+        .attr("height", d => 49)
+        .attr("y", (d, i) => i * 50)
+        .attr("fill", (d) => colorScale(d))
+        .attr("transform", "translate(0,5)");
---
+++ tree_basic/tree_basic.js
@@ -0,0 +1,55 @@
+window.onload = function () {
+
+    const data = {
+        "name": "1",
+        "children": [
+            {
+                "name": "1.1",
+                "children": [{ "name": "1.1.1" },
+                { "name": "1.1.2" },
+                { "name": "1.1.3" }]
+            },
+            {
+                "name": "1.2"
+            }]
+    };
+
+    // Creating hierarchal model and getting reference to root node 
+    const root = d3.hierarchy(data);
+
+    // Create treeLayout function and setting the size
+    const treeLayout = d3.tree().size([580, 80]);
+
+    // Creating a tree model
+    let tree = treeLayout(root);
+
+    // Append children svg circles
+    d3.select("svg g#circles")
+        .selectAll("circles")
+        .data(tree.descendants()) // descendants returns all children nodes
+        .enter()
+        .append("circle")
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+        .attr("r", 7);
+
+    //Append link lines between circles
+    d3.select("svg g#lines")
+        .selectAll("line")
+        .data(tree.links()) // links returns all lines svg coordinates connecting the nodes 
+        .enter()
+        .append("line")
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
+
+    d3.select("svg g#info")
+        .selectAll("text")
+        .data(tree.descendants())
+        .enter()
+        .append("text")
+        .text(d => d.data.name)
+        .attr("x", d => d.x + 10)
+        .attr("y", d=> d.y + 5)
+}
